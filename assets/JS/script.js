96230a3b41c02ddf551c5b35808334ad153c3bc7
Valentin <vmars2323@gmail.com>
2021-09-26 15:35:27 +0300

    bugfix modal

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -4,7 +4,7 @@ function slider(nav, block, shift) {
 
     function removing(btn) {
         btn.forEach(function(item) {
-            item.classList.remove('active');})
+            item.classList.remove('active');});
         }
 
         for (let i=0; i<btn.length;i++) {
@@ -12,7 +12,7 @@ function slider(nav, block, shift) {
                 removing(btn);
                 blk.style.left = i*(shift) + 'px';
                 btn[i].classList.add('active');
-            }) 
+            }); 
         }
 }
 
@@ -32,3 +32,98 @@ slider('.sliderTarif__toggle', '.tarif__slider', -350);
 // Slider в блоке Steps
 
 slider('.sliderSteps__toggle', '.steps__sliderblock', -243);
+
+
+// Модальное окно
+
+const signInBtn = document.querySelector('.header__signin'),
+      signInPopup = document.querySelector('.signInPopup'),
+      logInBtn = document.querySelector('.header__login'),
+      logInPopup = document.querySelector('.logInPopup'),
+      closeSignIn = document.querySelector('#closeSignIn'),
+      closeLogIn = document.querySelector('#closeLogIn');
+
+      
+    function openPopup(e, popup) {
+        e.preventDefault();
+        popup.classList.remove('closePopup');
+        popup.classList.add('showPopup');
+        document.body.style.overflow = 'hidden'; // Убираем прокрутку
+    }
+
+      signInBtn.addEventListener('click', (e) => {
+        openPopup(e, signInPopup);
+      });
+
+      logInBtn.addEventListener('click', (e) => {
+        openPopup(e, logInPopup);
+    });
+
+// Закрываем модальные окна
+
+function closePopup(e, popup) {
+    e.preventDefault();
+    popup.classList.add('closePopup');
+    popup.classList.remove('showPopup');
+    document.body.style.overflow = ''; // Возвращаем прокрутку
+}
+
+closeSignIn.addEventListener('click', (e) => {
+    closePopup(e, signInPopup);
+});
+
+closeLogIn.addEventListener('click', (e) => {
+    closePopup(e, logInPopup);
+});
+
+// Закрытие модального окна при нажатии на область вне окна
+
+signInPopup.addEventListener('click', (e) => {
+    if (e.target === signInPopup) {
+        closePopup(e, signInPopup);
+    }
+});
+
+logInPopup.addEventListener('click', (e) => {
+    if (e.target === logInPopup) {
+        closePopup(e, logInPopup);
+    }
+});
+
+// Анимация модальных окон при 375px
+// Открытие бургер-меню
+
+const headerPopup = document.querySelector('.header__popup'),
+      burger = document.querySelector('.header__burger'),
+      closeBurger = document.querySelector('.header__popup .popup__close'),
+      headerLogin = document.querySelector('.header__popup .header__login'),
+      headerSignin = document.querySelector('.header__popup .header__signin');
+
+
+function openPopupMini(e, popup) {
+    e.preventDefault();
+    popup.classList.remove('header__popupClose');
+    popup.classList.add('header__popupShow');
+}
+
+burger.addEventListener('click', (e) => {
+    e.preventDefault();
+    headerPopup.classList.remove('header__popupClose');
+    headerPopup.classList.add('header__popupShow');
+});
+
+closeBurger.addEventListener('click', (e) => {
+    e.preventDefault();
+    headerPopup.classList.add('header__popupClose');
+    headerPopup.classList.remove('header__popupShow');
+});
+
+// Открытие модальных окон при 375px
+
+headerLogin.addEventListener('click', (e) => {
+    openPopup(e, signInPopup);
+});
+
+headerSignin.addEventListener('click', (e) => {
+    openPopup(e, logInPopup);
+});
